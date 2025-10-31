386ce3cfb4fee68496843d8e81bf8cd31cdba679
alejogiga05-prog <alejogiga05@gmail.com>
2025-10-31 18:12:15 -0500

    Update appfinal.py

--- appfinal.py
+++ appfinal.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import InfluxDBClient
+import influxdb_client
 from sklearn.linear_model import LinearRegression
 from dotenv import load_dotenv
 import os
